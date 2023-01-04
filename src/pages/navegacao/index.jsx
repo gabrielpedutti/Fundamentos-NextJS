123e4ed4d1ab0ad88396ed1c98d3833b3db82a8b
gabrielpedutti <gabrielpedutti@hotmail.com>
2023-01-03 22:25:06 -0300

    Entendendo pré renderização

--- src/pages/navegacao/index.jsx
+++ src/pages/navegacao/index.jsx
@@ -4,7 +4,7 @@ export default function Navegacao() {
     return (
         <Layout titulo="Exemplo de Navegação #01">
             <h1>Navegação #01</h1>
-            <p>Como esse arquivo se chama index, na hora de escrever o caminho para o mesmo, basta escrever o nome da pasta, visto que "/navegacao/" cairá no index da página.</p>
+            {/* <p>Como esse arquivo se chama index, na hora de escrever o caminho para o mesmo, basta escrever o nome da pasta, visto que "/navegacao/" cairá no index da página.</p> */}
         </Layout>
     )
 }
